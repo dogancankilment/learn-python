047da54e162c0d2a9a53a0aaf5496d1347dc1d48
Doğan Can Kilment <dogancankilment@users.noreply.github.com>
2018-01-07 05:05:07 +0300

    ders-1 file added

---
+++ ders-1.py
@@ -0,0 +1,30 @@
+# python
+
+# ekrana yazdirma komutlari
+print "dogancankilment"
+
+# utf-8 hata verecektir, python dosyasi icersinde
+# python komut satirinda calistirilabilir
+print "doğancankilment"
+
+# tirnak ve ozel karakterden kacis
+print 'dogancankilment'
+print " utopian'dan dogancankilment "
+print """ utopian'dan "dogancankilment" """
+print """ utopian'dan dogancankilment """
+print " utopian\'dan dogancankilment "
+print "dogancankilment \n utopian \n steemit"
+
+# aritmetiksel islemler
+sayi1 = 5
+sayi2 = 10
+sayi1 + sayi2
+print sayi1 + sayi2
+print "toplam: ", sayi1+sayi2
+
+sayi3 = 10
+type(sayi3)
+sayi3 = "10"
+type(sayi3)
+toplam = int(sayi3) + sayi1
+print "toplam: ", toplam
