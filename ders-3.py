77f75ebce29dfbe77d2c5c6a374f47fe851eaab0
Doğan Can Kilment <dogancankilment@users.noreply.github.com>
2018-01-09 11:41:20 +0300

    Create ders-3.py

---
+++ ders-3.py
@@ -0,0 +1,45 @@
+kontrol_sayimiz = 8
+karsilastir_sayisi = 10
+
+if kontrol_sayimiz == karsilastir_sayisi:
+    print "sayilar esit"
+else:
+    print "sayilar esit degil"
+
+if kontrol_sayimiz < karsilastir_sayisi:
+    print "sayimiz kontrol sayimizdan kucuktur"
+else:
+    print "sayimiz kontrol sayimizdan buyuktur"
+
+if kontrol_sayimiz < 10:
+    print "10'dan kucuktur"
+elif kontrol_sayimiz > 5:
+    print "5'ten buyuktur"
+else:
+    print "ikisi de degil"
+
+if kontrol_sayimiz < 10 and kontrol_sayimiz > 5:
+    print "10'dan kucuk ve 5'ten buyuk"
+else:
+    print "hicbirisi"
+
+1 or 0 //// 0 or 1
+
+test_sayimiz = 5
+if test_sayimiz == 5:
+    print "sayimiz 5tir"
+
+if test_sayimiz != 6:
+    print "sayimiz 6 dan farklıdır"
+
+if test_sayimiz > 4:
+    print "sayimiz 4 ten buyuktur"
+
+if test_sayimiz < 6:
+    print "sayimiz 6 dan kucuktur"
+
+bool() ///// boolean
+bool("")
+bool(1)
+bool("dogancankilment from utopian-io")
+bool(0)
