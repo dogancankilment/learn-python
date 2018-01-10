a3fe4aba63d98810c3906f09085e3885ba720dd0
Doğan Can Kilment <dogancankilment@users.noreply.github.com>
2018-01-11 02:45:59 +0300

    Created ders-4.py

---
+++ ders-4.py
@@ -0,0 +1,32 @@
+degiskenimiz = "dogancankilment"
+for her_bir_harf in degiskenimiz:
+	print her_bir_harf
+
+range(baslangic_degeri, bitis_degeri)
+print range(0,100)
+
+for her_bir_sayi in range(0,5):
+	print her_bir_sayi
+
+dizi = range(0,5)
+print dizi
+len(dizi)
+
+for dizi_elemani in dizi:
+	print dizi_elemani
+
+dizi[0:3]
+dizi[0:2]
+while True:
+	print "sonsuza dek yazacak"
+
+while kontrol_sayimiz < 10:
+	kontrol_sayimiz = kontrol_sayimiz +1
+	print "kontrol_sayimiz: ", kontrol_sayimiz
+
+# continue  /// break
+
+while kontrol_sayimiz < 10:
+	kontrol_sayimiz = kontrol_sayimiz +1
+	print "kontrol_sayimiz: ", kontrol_sayimiz
+	break;
